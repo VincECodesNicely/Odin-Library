8b426a6d2fe395d3af7ac2a00baf4eac7ce5b427
VincECodesNicely <vk6953@hotmail.com>
2025-09-08 12:11:41 +1200

    add book function

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary(title, author, numOfPages, readStatus) {
+  // take params, create a book then store it in the array
+}
